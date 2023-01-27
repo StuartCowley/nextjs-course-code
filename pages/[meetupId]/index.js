717b7907db8bd42ac8c06317e7affa7233a82ec0
StuartCowley <voidstalker@live.co.uk>
2023-01-27 14:40:58 +0000

    Create MeetupDetails component with modules styles, add to MeetupDetailsPage

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,5 +1,14 @@
+import MeetupDetails from "../../components/meetups/MeetupDetails";
+
 const MeetupDetailsPage = () => {
-  return <div>MeetupDetailsPage</div>;
+  return (
+    <MeetupDetails
+      image="https://cdn.britannica.com/35/155335-050-D0C61BB7/Notre-Dame-de-Paris-France.jpg"
+      title="A first meetup"
+      address="An address, toBeFilledOutLater drive"
+      description="The description"
+    />
+  );
 };
 
 export default MeetupDetailsPage;
