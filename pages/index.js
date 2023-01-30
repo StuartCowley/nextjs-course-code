6318a18c1cfe27315e27877a53640429050939ca
StuartCowley <voidstalker@live.co.uk>
2023-01-30 12:04:16 +0000

    Populate getStaticProps with data from mongo, remove dummy meetups

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,6 @@
-import MeetupList from "../components/meetups/MeetupList";
+import { MongoClient } from "mongodb";
 
-const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    title: "A first meetup",
-    image:
-      "https://cdn.britannica.com/35/155335-050-D0C61BB7/Notre-Dame-de-Paris-France.jpg",
-    address: "12344 Lester Drive, Somewhere, UX43 9KS",
-    description: "This is the first meetup!",
-  },
-  {
-    id: "m2",
-    title: "A second meetup",
-    image:
-      "https://cdn.britannica.com/35/155335-050-D0C61BB7/Notre-Dame-de-Paris-France.jpg",
-    address: "643 Aigburth Road, Merseyside, L31 5LW",
-    description: "This is the second meetup!",
-  },
-];
+import MeetupList from "../components/meetups/MeetupList";
 
 const HomePage = (props) => {
   return <MeetupList meetups={props.meetups} />;
@@ -25,11 +8,26 @@ const HomePage = (props) => {
 
 export async function getStaticProps() {
   // fetch data from an API
+  // Rather than creating a new /api/getMeetups route this can be done
+  // directly here as it will always be kept on the server side
+  const client = await MongoClient.connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@atlascluster.c3dyrvm.mongodb.net/meetups?retryWrites=true&w=majority`
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+  const meetups = await meetupsCollection.find().toArray();
+  client.close();
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS
-    }
-  }
+      meetups: meetups.map((meetup) => ({
+        id: meetup._id.toString(),
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+      })),
+    },
+  };
 }
 
 export default HomePage;
